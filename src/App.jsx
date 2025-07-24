ed41aa6c16fca0bdca1d6ad107e9449a4465506c
LucaCiani <lucaciani88@gmail.com>
2025-07-24 16:30:43 +0200

    scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./assets/layouts/DefaultLayout";
+import HomePage from "./assets/components/HomePage";
+import Products from "./assets/components/Products";
+import AboutUs from "./assets/components/AboutUs";
 
 function App() {
-    return <></>;
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route Component={DefaultLayout}>
+                        <Route path="/" Component={HomePage} />
+                        <Route path="/products" Component={Products} />
+                        <Route path="/about-us" Component={AboutUs} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
