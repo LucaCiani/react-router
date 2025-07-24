e6ec211241833f178cef698b17592c1054cc28bc
LucaCiani <lucaciani88@gmail.com>
2025-07-24 16:54:35 +0200

    add header

--- src/assets/layouts/DefaultLayout.jsx
+++ src/assets/layouts/DefaultLayout.jsx
@@ -1,11 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 
 export default function DefaultLayout() {
     return (
         <>
-            <header>header</header>
-            <Outlet />
-            <footer>footer</footer>
+            <div className="container my-5">
+                <header className="my-5 d-flex align-items-center justify-content-center gap-5">
+                    <h1>MY SHOP🛒</h1>
+                    <nav className="nav">
+                        <NavLink className="nav-link " to="/">
+                            Home🏠
+                        </NavLink>
+                        <NavLink className="nav-link" to="/products">
+                            Products📦
+                        </NavLink>
+                        <NavLink className="nav-link" to="/about-us">
+                            About Usℹ️
+                        </NavLink>
+                    </nav>
+                </header>
+                <Outlet />
+            </div>
         </>
     );
 }
